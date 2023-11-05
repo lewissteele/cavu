9410ef70d658eb0596bcdde82e2619c00b381d0b
Lewis Steele <lewissteele254@gmail.com>
2023-11-05 17:16:32 +0000

    Implement car park index

--- app/Http/Controllers/CarParkController.php
+++ app/Http/Controllers/CarParkController.php
@@ -5,61 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCarParkRequest;
 use App\Http\Requests\UpdateCarParkRequest;
 use App\Models\CarPark;
+use Illuminate\Http\JsonResponse;
 
 class CarParkController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json(CarPark::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreCarParkRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(CarPark $carPark)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(CarPark $carPark)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateCarParkRequest $request, CarPark $carPark)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(CarPark $carPark)
+    public function show(CarPark $carPark): JsonResponse
     {
         //
     }
