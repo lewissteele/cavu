1c02192c4d006aae3a31375d4777d02d2661e4e2
Lewis Steele <lewissteele254@gmail.com>
2023-11-05 16:11:01 +0000

    Create model for car park

---
+++ app/Http/Controllers/CarParkController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCarParkRequest;
+use App\Http\Requests\UpdateCarParkRequest;
+use App\Models\CarPark;
+
+class CarParkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCarParkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CarPark $carPark)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CarPark $carPark)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCarParkRequest $request, CarPark $carPark)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CarPark $carPark)
+    {
+        //
+    }
+}
