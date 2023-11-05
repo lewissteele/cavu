a6b4269626b79cddb058b451ad6d03d1b11c527e
Lewis Steele <lewissteele254@gmail.com>
2023-11-05 17:15:12 +0000

    Create test for car park index

--- tests/Feature/CarParkControllerTest.php
+++ tests/Feature/CarParkControllerTest.php
@@ -2,19 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Models\CarPark;
+use App\Models\ParkingSpace;
+use App\Models\Price;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CarParkControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_index_returns_all_car_parks(): void
+    {
+        $this->seed();
+
+        $this->getJson('api/car-park')
+            ->assertSuccessful()
+            ->assertJsonCount(CarPark::all()->count());
     }
 }
