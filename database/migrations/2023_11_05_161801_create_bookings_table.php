ff1ded72d1d0f6886983aebf79f9d82353bb131a
Lewis Steele <lewissteele254@gmail.com>
2023-11-05 16:20:17 +0000

    Add columns for bookings table

--- database/migrations/2023_11_05_161801_create_bookings_table.php
+++ database/migrations/2023_11_05_161801_create_bookings_table.php
@@ -1,25 +1,23 @@
 <?php
 
+use App\Models\ParkingSpace;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(ParkingSpace::class)->constrained();
+            $table->date('from');
+            $table->date('to');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('bookings');
