b1e442713bf520ff8f6241ba09149c20c17d7c2c
Lewis Steele <lewissteele254@gmail.com>
2023-11-05 16:27:13 +0000

    Add price columns

--- database/migrations/2023_11_05_162323_create_prices_table.php
+++ database/migrations/2023_11_05_162323_create_prices_table.php
@@ -1,25 +1,23 @@
 <?php
 
+use App\Models\CarPark;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('prices', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(CarPark::class)->constrained();
+            $table->date('date');
+            $table->float('price');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('prices');
