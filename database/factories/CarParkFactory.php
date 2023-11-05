19b172a532002297e3f68266da40e3f1bac6532b
Lewis Steele <lewissteele254@gmail.com>
2023-11-05 16:33:36 +0000

    Tidy up factories

--- database/factories/CarParkFactory.php
+++ database/factories/CarParkFactory.php
@@ -9,15 +9,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CarParkFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
-        return [
-            //
-        ];
+        return [];
     }
 }
